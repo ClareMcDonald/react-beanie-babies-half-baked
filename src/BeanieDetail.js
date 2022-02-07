a4c2c672c51ed2d73633ec0a2a30a2613b7eb4a3
ClareMcDonald <clare.s.mcdonald@gmail.com>
2022-02-07 14:41:39 -0800

    wrote BeanieDetail function with useEffect

--- src/BeanieDetail.js
+++ src/BeanieDetail.js
@@ -1,14 +1,23 @@
 import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { getSingleBeanie } from './services/fetch-utils';
 
 export default function BeanieDetail() {
   // you'll need to track the current beanieBaby in state
+  const [beanieBaby, setBeanieBaby] = useState({});
+
   // you'll need to get the route param using the appropriate react router hook
+  const params = useParams();
 
   useEffect(() => {
     // you'll need to define a fetch function here (then call it below) that gets this page's beanie baby and injects it into state using the correct state handler
+    async function onLoad() { 
+      const data = await getSingleBeanie(params.id);
 
+      setBeanieBaby(data);
+    }
+
+    onLoad();
   }, []); // note that you'll want the id from the url in the dependency array because you want the useEffect callback to get called every time the url changes 
 
   function handleBeanieClick() {
